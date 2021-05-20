6cbd212f80fb52a306abbafae1d93d2b4c8c867c
Florian Defer <florian.defer@hotmail.fr>
2021-05-20 15:42:42 +0200

    port listening

--- server.js
+++ server.js
@@ -39,15 +39,9 @@ console.log('3')
 const server = http.createServer(app);
 console.log('4')
 server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
-});
-console.log('5')
-//server.listen();
-server.listen(port, function(err) {
-  if(err) console.log( err);
-  
 
-})
+
+
+server.listen(3000,function(){  
+  console.log('Express app start on port 3000')  
+});
