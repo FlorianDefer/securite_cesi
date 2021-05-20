0ed1db6b2e6842f2308e2a26df94fefd5e4e65bb
Florian Defer <florian.defer@hotmail.fr>
2021-05-20 13:52:53 +0200

    test db connexion

--- _helpers/db.js
+++ _helpers/db.js
@@ -3,12 +3,12 @@ console.log("file mongoose")
 const mongoose = require('mongoose');
 const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
 console.log("try to connect")
-//mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions).then(() => {
-//    console.log("Connected to MongoDB successfully :)");
-//}).catch((e) => {
-//    console.log("Error while attempting to connect to MongoDB");
-//    console.log(e);
-//});
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions).then(() => {
+    console.log("Connected to MongoDB successfully :)");
+}).catch((e) => {
+    console.log("Error while attempting to connect to MongoDB");
+    console.log(e);
+});
 mongoose.Promise = global.Promise;
 
 module.exports = {
--- app.js
+++ app.js
@@ -64,11 +64,11 @@ app.use(cookieParser());
 
 app.use(morgan('combined', { stream: winston.stream }));
 
-mongoose.connect('mongodb://localhost:27017',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+//mongoose.connect('mongodb://localhost:27017',
+//  { useNewUrlParser: true,
+//    useUnifiedTopology: true })
+//  .then(() => console.log('Connexion à MongoDB réussie !'))
+//  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 
