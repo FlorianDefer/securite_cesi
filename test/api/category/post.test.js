e745569af98b6505f6ac0d3f7ab90979a8e3ce4a
Andres IGEA OVIEDO <andres.igeaoviedo@amadeus.com>
2021-06-03 18:46:53 +0200

    4th run with post testing

--- test/api/category/post.test.js
+++ test/api/category/post.test.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = 'test';
 
 const app = require('../../../app.js');
+const accountService = require('../../accounts/account.service');
 //const conn = require('../../../db/index.js');
 const request = require('supertest');
 
@@ -23,7 +24,33 @@ describe('POST /category/create', () => {
 //   .catch((err) => done(err));
 // })
 
+//statusCode: 401,
+//statusMessage: 'Unauthorized',
+
 it('OK, creating a new category works', (done) => {
+
+//First create admin account
+const accountAdmin = {
+  id: '134ced02ed345b2b049232c7',
+  title: 'Mr',
+  firstName: 'Admin',
+  lastName: 'Account',
+  email: 'admin.account@gmail.com',
+  role: 'Admin',
+  password: 'YsiteProvoka!342'
+};
+
+const accountAdminCreated = await accountService.create(accountAdmin);
+
+//{ email, password, ipAddress }
+//Then authenticate
+request(app).post('accounts/authenticate')
+  .send({ email: accountAdminCreated.email, password: accountAdmin.password})
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => done(err));
+
 
 request(app).post('/category/create')
   .send({ name: 'Health', _userId: 1 })
