8e92c41c7adee56503a8ca38bea472dba07bb4dd
AnisCesi <72912487+AnisCesi@users.noreply.github.com>
2021-06-03 21:12:24 +0200

    Unittesting comments

--- test/api/comment/post.test.js
+++ test/api/comment/post.test.js
@@ -11,17 +11,17 @@ chai.use(require('sinon-chai'));
 const expect = chai.expect;
 
 
-describe('Integration test for: POST /category/create', () => {
+describe('Integration test for: POST /comment', () => {
 
-it('Should Create a new category',  async () => {
+it('Should Create a new comment',  async () => {
 
 //First create admin account
 const accountAdmin = {
-  id: '134ced02ed345b2b049232c7',
+  id: '134ced02ed345b2b049232c7comment',
   title: 'Mr',
   firstName: 'Admin',
   lastName: 'Account',
-  email: 'admin.account@gmail.com',
+  email: 'admin.accountcomment@gmail.com',
   role: 'Admin',
   password: 'YsiteProvoka!342'
 };
@@ -33,26 +33,25 @@ const accountAdminCreated = await accountService.create(accountAdmin);
 request(app).post('accounts/authenticate')
   .send({ email: accountAdminCreated.email, password: accountAdmin.password})
   .then((res) => {
-    //console.log(res);
+   // console.log(res);
   })
   .catch((err) => done(err));
 
-//Create category
-request(app).post('/category/create')
-  .send({ name: 'Health', _userId: 1 })
+//Create comment
+request(app).post('/comment')
+  .send({ content: 'Je suis un commentaire', dateCreated: Date.now, public: true, _userId: accountAdminCreated.id })
   .then((res) => {
     const body = res.body;
     expect(body).to.contain.property('message');
-    done();
   })
 });
 
-it('Should not create a category for an unauthenticated user', async () => {
-request(app).post('/category/create')
-  .send({ name: 'Health' })
+it('Should not create a comment for an unauthenticated user', async () => {
+request(app).post('/comment')
+  .send({ content: 'Je suis un commentaire', dateCreated: Date.now, public: true, _userId: 123456 })
   .then((res) => {
     const body = res.body;
-   // console.log(res);
+    //console.log(res);
     expect(body.status).to.equal(401);
     //expect(res.statusMessage).to.equal('Unauthorized');
     done();
@@ -61,9 +60,9 @@ request(app).post('/category/create')
 });
 
 
-it('Should not create a category without a user ID', async () => {
-request(app).post('/category/create')
-  .send({ name: 'Health' })
+it('Should not create a comment without a user ID', async () => {
+request(app).post('/comment')
+  .send({ content: 'Je suis un commentaire', dateCreated: Date.now, public: true })
   .then((res) => {
     const body = res.body;
     expect(body.errors).to.equal('ValidatorError');
